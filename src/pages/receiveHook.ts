16af557a794df353d6459e80a47d91083bfeef9f
Luiz Ferraz <luiz@lferraz.com>
2024-03-06 23:19:33 -0300

    WIP

--- src/pages/receiveHook.ts
+++ src/pages/receiveHook.ts
@@ -2,10 +2,21 @@ import type { APIRoute } from 'astro';
 import { db, WebHooks } from 'astro:db';
 
 export const ALL: APIRoute = async (context) => {
-  await db.insert(WebHooks).values({
-    content: await context.request.json(),
-    headers: context.request.headers,
-  });
+  try {
+    await db.insert(WebHooks).values({
+      content: await context.request.json(),
+      headers: context.request.headers,
+    });
+  } catch (e: any) {
+    return new Response(JSON.stringify({
+      error: e.message,
+    }), {
+      status: 500,
+      headers: {
+        'content-type': 'application/json',
+      }
+    });
+  }
 
   return new Response('OK', {
     status: 200,
